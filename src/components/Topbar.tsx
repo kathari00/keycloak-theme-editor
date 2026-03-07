d0e0e0a80b952780a5ac27ee9578c00c9ddcaef2
kathari00 <katharina.eiserfey1@gmail.com>
2026-03-07 19:43:29 +0100

    Move introduction deck and add About link

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,5 +1,5 @@
 import { Button, FormSelect, FormSelectOption } from '@patternfly/react-core'
-import { MoonIcon, SunIcon } from '@patternfly/react-icons'
+import { ExternalLinkAltIcon, MoonIcon, SunIcon } from '@patternfly/react-icons'
 import { editorActions } from '../features/editor/actions'
 import { useDarkModeState, usePreviewState } from '../features/editor/hooks/use-editor'
 import { cx } from '../lib/cx'
@@ -13,6 +13,16 @@ export default function Topbar({
   return (
     <div className={cx('gjs-top-sidebar flex flex-wrap items-start justify-between gap-2 p-1', className)}>
       <div className="flex items-center gap-2 flex-shrink-0">
+        <Button
+          variant="secondary"
+          component="a"
+          href="/docs/presentation/introduction/index.html"
+          target="_blank"
+          icon={<ExternalLinkAltIcon />}
+          style={{ whiteSpace: 'nowrap', paddingInline: '2rem' }}
+        >
+          About
+        </Button>
         <FormSelect
           value={deviceId}
           onChange={(_event, value) => editorActions.setDeviceId(value as 'desktop' | 'tablet' | 'mobile')}
