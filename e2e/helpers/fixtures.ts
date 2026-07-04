2e85434b4db87d7f81614670c597c40b274d425a
kathari00 <katharina.eiserfey1@gmail.com>
2026-07-04 16:34:17 +0200

    Add E2E happy path workflows

---
+++ e2e/helpers/fixtures.ts
@@ -0,0 +1,4 @@
+export const svgLogo = `<svg xmlns="http://www.w3.org/2000/svg" width="64" height="32" viewBox="0 0 64 32">
+  <rect width="64" height="32" fill="#13579b"/>
+  <text x="8" y="21" fill="#fff" font-family="Arial" font-size="12">E2E</text>
+</svg>`
